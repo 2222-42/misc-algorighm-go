76a87947f55ca638f5a2c24c500daea977e5a8d0
2222-42 <stephen0617@me.com>
2021-07-17 17:00:23 +0900

    test(longest-substring): test cases

---
+++ longest-substring/lengthOfLongestSubstring_test.go
@@ -0,0 +1,37 @@
+package longest_substring
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	testCases := []struct {
+		in   string
+		want int
+	}{
+		{
+			in:   "abcabcbb",
+			want: 3,
+		},
+		{
+			in:   "bbbbb",
+			want: 1,
+		},
+		{
+			in:   "pwwkew",
+			want: 3,
+		},
+		{
+			in:   "",
+			want: 0,
+		},
+	}
+
+	for _, test := range testCases {
+		result1 := LengthOfLongestSubstring(test.in)
+		if !reflect.DeepEqual(test.want, result1) {
+			t.Errorf("in: %v, expected: %v, result: %v", test.in, test.want, result1)
+		}
+	}
+}
