c1603524f98aa3b3ab21531c3f3e66101320c997
2222-42 <stephen0617@me.com>
2021-08-29 11:27:25 +0900

    refactor(longest_palindrome): kata

--- longest-palindrome/longestPalindrome.go
+++ longest-palindrome/longestPalindrome.go
@@ -1,10 +1,5 @@
 package longest_palindrome
 
-import (
-	"fmt"
-	"strings"
-)
-
 func LongestPalindrome(s string) string {
 	/* 方針1:
 	stringから部分を取っていく
@@ -19,49 +14,23 @@ func LongestPalindrome(s string) string {
 		確認する方法は、真ん中の文字をとって、そこから比較していく。
 	*/
 
-	sliceOfString := strings.Split(s, "")
-
-	candidates := []string{}
-
-	for j := 0; j < len(s); j++ {
-		targetSlice := sliceOfString[j:]
-		stocks := map[string]int{}
-		candidate := ""
-		for i, c := range targetSlice {
-			candidate = candidate + c
-			if i == 0 {
-				stocks[c] = i
-				candidates = append(candidates, candidate)
-				continue
-			}
-
-			if _, ok := stocks[c]; ok {
-				candidates = append(candidates, candidate)
-				continue
-			}
-			stocks[c] = i
-		}
-	}
-
-	newCandidates := []string{}
-	for _, comp := range candidates {
-		lenOfComp := len(comp)
-		simplified := ""
-		med := (lenOfComp / 2)
-		if lenOfComp%2 == 0 {
-			simplified = checkValidString(comp, med-1, med)
-		} else {
-			simplified = checkValidString(comp, med, med)
-		}
-
-		newCandidates = append(newCandidates, simplified)
-	}
+	/* 方針3:
+	for分回して、真ん中の文字をとって、そこから比較していく。
+	*/
 
 	result := ""
-	maxLength := 0
-	for _, candidate := range newCandidates {
-		if len(candidate) > maxLength {
-			maxLength = len(candidate)
+
+	for i := 0; i < len(s); i++ {
+		even := checkValidString(s, i, i+1)
+		odd := checkValidString(s, i, i)
+		candidate := ""
+		if len(odd) > len(even) {
+			candidate = odd
+		} else {
+			candidate = even
+		}
+
+		if len(candidate) > len(result) {
 			result = candidate
 		}
 	}
@@ -70,12 +39,11 @@ func LongestPalindrome(s string) string {
 }
 
 func checkValidString(s string, start, end int) string {
-	fmt.Printf("start: %d, end: %d\n", start, end)
+
 	for start >= 0 && end <= len(s)-1 && s[start] == s[end] {
 		start -= 1
 		end += 1
 	}
-	fmt.Printf("start: %d, end: %d\n", start, end)
 
 	return s[start+1 : end]
 }
