6419fb34676619de9d316c1d4fbce67d2a135438
2222-42 <stephen0617@me.com>
2021-09-13 19:07:56 +0900

    chore(slice): json marshal case

--- slice-of-empty-or-nil/NilOrEmptySlice.go
+++ slice-of-empty-or-nil/NilOrEmptySlice.go
@@ -1,18 +1,22 @@
 package slice_of_empty_or_nil
 
+// nil slice
 func ReturnNilSlice() []string {
 	return nil
 }
 
+// empty slice
 func ReturnEmptySlice() []string {
 	return []string{}
 }
 
+// nil slice
 func ReturnInitialSlice() []string {
 	var result []string
 	return result
 }
 
+// empty slice
 func ReturnMadeSlice() []string {
 	return make([]string, 0, 0)
 }
--- slice-of-empty-or-nil/NilOrEmptySlice_test.go
+++ slice-of-empty-or-nil/NilOrEmptySlice_test.go
@@ -1,38 +1,90 @@
 package slice_of_empty_or_nil
 
 import (
+	"encoding/json"
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestCheck(t *testing.T) {
-	result1 := ReturnNilSlice()     // return nil
-	result2 := ReturnEmptySlice()   // return []string{}
-	result3 := ReturnInitialSlice() // use var
-	result4 := ReturnMadeSlice()    // use make
+	s1 := ReturnNilSlice()     // return nil
+	s2 := ReturnEmptySlice()   // return []string{}
+	s3 := ReturnInitialSlice() // use var
+	s4 := ReturnMadeSlice()    // use make
 
 	// nil と emptyは等しくない
-	if reflect.DeepEqual(result1, result2) {
-		t.Errorf("expected: %v, result: %v", result1, result2)
+	if reflect.DeepEqual(s1, s2) {
+		t.Errorf("expected: %v, result: %v", s1, s2)
 	}
 
 	// nil と initialは等しい
-	if !reflect.DeepEqual(result1, result3) {
-		t.Errorf("expected: %v, result: %v", result3, result1)
+	if !reflect.DeepEqual(s1, s3) {
+		t.Errorf("expected: %v, result: %v", s3, s1)
 	}
 
 	// emptyとvarで作ったのは等しくない
-	if reflect.DeepEqual(result2, result3) {
-		t.Errorf("expected: %v, result: %v", result3, result2)
+	if reflect.DeepEqual(s2, s3) {
+		t.Errorf("expected: %v, result: %v", s3, s2)
 	}
 
 	// emptyとmakeで作ったのは等しい
-	if !reflect.DeepEqual(result2, result4) {
-		t.Errorf("expected: %v, result: %v", result4, result2)
+	if !reflect.DeepEqual(s2, s4) {
+		t.Errorf("expected: %v, result: %v", s4, s2)
 	}
 
 	// varで作ったのとmakeで作ったのとは等しくない
-	if reflect.DeepEqual(result3, result4) {
-		t.Errorf("expected: %v, result: %v", result4, result2)
+	if reflect.DeepEqual(s3, s4) {
+		t.Errorf("expected: %v, result: %v", s4, s2)
 	}
+
+	if s1[:] != nil {
+		t.Errorf("s1 should not be nil")
+	}
+
+	if s2[:] == nil {
+		t.Errorf("s2 should be nil")
+	}
+
+	if s3[:] != nil {
+		t.Errorf("s3 should not be nil")
+	}
+
+	if s4[:] == nil {
+		t.Errorf("s4 should be nil")
+	}
+
+	jsonBytes1, err := json.Marshal(s1)
+	if err != nil {
+		fmt.Println("JSON Marshal error:", err)
+		return
+	}
+	if string(jsonBytes1) != "null" {
+		t.Error("json.Marshal of s1 should be null")
+	}
+
+	jsonBytes2, err := json.Marshal(s2)
+	if err != nil {
+		t.Error("JSON Marshal error:", err)
+	}
+	if string(jsonBytes2) != "[]" {
+		t.Errorf("json.Marshal of s2 should be []")
+	}
+
+	jsonBytes3, err := json.Marshal(s3)
+	if err != nil {
+		t.Error("JSON Marshal error:", err)
+	}
+	if string(jsonBytes3) != "null" {
+		t.Errorf("json.Marshal of s1 should be null")
+	}
+
+	jsonBytes4, err := json.Marshal(s4)
+	if err != nil {
+		t.Error("JSON Marshal error:", err)
+	}
+	if string(jsonBytes4) != "[]" {
+		t.Errorf("json.Marshal of s4 should be []")
+	}
+
 }
